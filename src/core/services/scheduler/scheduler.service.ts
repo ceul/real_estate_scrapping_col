4a43a4cbb65c236b5a6017ee25e7d901f803b9d3
“currego” <currego@splunk.com>
2022-06-29 11:24:36 -0500

    Se oprganiza el scheduler para que traiga todas las ciudades de la base de datos

--- src/core/services/scheduler/scheduler.service.ts
+++ src/core/services/scheduler/scheduler.service.ts
@@ -1,19 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Ciudad } from 'src/core/models/ciudad.entity';
+import { Repository } from 'typeorm';
 import { PropiedadService } from '../../business/propiedad-service';
 
 @Injectable()
 export class SchedulerService {
 
-    constructor(private propiedadService: PropiedadService) {
+    constructor(private propiedadService: PropiedadService,
+        @InjectRepository(Ciudad) private readonly ciudadRepsitory: Repository<Ciudad>
+    ) {
         this.scheduler()
     }
 
     async scheduler() {
         try {
-            this.propiedadService.getFromFincaRaiz()
-            await setTimeout( ()=> this.scheduler(),86400000)
+            console.log('scheduler arranco')
+            let ciudades = await this.ciudadRepsitory.find({
+                select: {
+                    id: true,
+                    nombre: true,
+                }
+            })
+            console.log(ciudades)
+            for (let index = 0; index < ciudades.length; index++) {
+                this.propiedadService.getFromFincaRaiz(ciudades[index].nombre)
+            }
+            await setTimeout(() => this.scheduler(), 86400000)
         } catch (error) {
             console.log(error)
         }
     }
+
 }
