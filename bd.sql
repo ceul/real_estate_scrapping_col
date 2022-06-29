31ceddd1811c7d13a4e3b56aca253a70fa315d55
“currego” <currego@splunk.com>
2022-06-29 11:22:41 -0500

    cambios a la bd

--- bd.sql
+++ bd.sql
@@ -5,55 +5,55 @@ CREATE DATABASE real_state;
 use real_state;
 
 CREATE TABLE departamento(
-    id SERIAL NOT NULL,
-    nombre VARCHAR(256),
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE ciudad(
-    id SERIAL NOT NULL,
-    nombre VARCHAR(256),
-    fk_departamento INT,
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(256) NOT NULL,
+    fk_departamento INT NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT fk_departamento FOREIGN KEY (id) 
+    FOREIGN KEY (fk_departamento) 
     REFERENCES departamento(id)
 );
 
 CREATE TABLE barrio(
-    id SERIAL NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(256),
-    fk_ciudad INT,
+    fk_ciudad INT NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT fk_ciudad FOREIGN KEY (id) 
+    CONSTRAINT fk_ciudad_barrio FOREIGN KEY (fk_ciudad) 
     REFERENCES ciudad(id)
 );
 
 CREATE TABLE tipo_negocio(
-    id SERIAL NOT NULL,
-    nombre VARCHAR(32),
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(32) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE tipo_propiedad(
-    id SERIAL NOT NULL,
-    nombre VARCHAR(32),
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(32) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE plataforma(
-    id SERIAL NOT NULL,
-    nombre VARCHAR(32),
-    url VARCHAR(512),
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(32) NOT NULL,
+    url VARCHAR(512) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE propiedad(
-    id SERIAL NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     id_plataforma VARCHAR(128),
     area DOUBLE(13,2) NOT NULL,
-    nro_cuartos VARCHAR(2),
-    nro_banos VARCHAR(2),
-    nro_garajes VARCHAR(4),
+    nro_cuartos INT,
+    nro_banos INT,
+    nro_garajes INT,
     latitud VARCHAR(4),
     longitud VARCHAR(4),
     valor_venta DOUBLE(13,2),
@@ -64,16 +64,22 @@ CREATE TABLE propiedad(
     fk_tipo_propiedad INT NOT NULL,
     fk_plataforma INT NOT NULL,
     fk_barrio INT NOT NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT fk_tipo_negocio FOREIGN KEY (id) 
-    REFERENCES tipo_negocio(id),
-    CONSTRAINT fk_tipo_propiedad FOREIGN KEY (id) 
-    REFERENCES tipo_propiedad(id),
-    CONSTRAINT fk_plataforma FOREIGN KEY (id) 
-    REFERENCES plataforma(id),
-    CONSTRAINT fk_barrio FOREIGN KEY (id) 
-    REFERENCES barrio(id)
+    PRIMARY KEY (id)
+    
 );
+
+
+    ALTER TABLE propiedad ADD CONSTRAINT fk_tipo_negocio FOREIGN KEY (fk_tipo_negocio) 
+    REFERENCES tipo_negocio(id);
+
+    ALTER TABLE propiedad ADD CONSTRAINT fk_tipo_propiedad FOREIGN KEY (fk_tipo_propiedad) 
+    REFERENCES tipo_propiedad(id);
+
+    ALTER TABLE propiedad ADD CONSTRAINT fk_plataforma FOREIGN KEY (fk_plataforma) 
+    REFERENCES plataforma(id);
+
+    ALTER TABLE propiedad ADD CONSTRAINT fk_barrio FOREIGN KEY (fk_barrio) 
+    REFERENCES barrio(id);
 
 INSERT INTO tipo_negocio (nombre) VALUES ('venta');
 INSERT INTO tipo_negocio (nombre) VALUES ('alquiler');
@@ -85,6 +91,20 @@ INSERT INTO tipo_propiedad (nombre) VALUES ('local');
 
 INSERT INTO plataforma (nombre, url) VALUES ('finca raiz', 'www.fincaraiz.com.co');
 INSERT INTO plataforma (nombre, url) VALUES ('metrocuadrado', 'www.metrocuadrado.com');
+
+INSERT INTO departamento (nombre) VALUES ('antioquia');
+INSERT INTO departamento (nombre) VALUES ('valle del cauca');
+INSERT INTO departamento (nombre) VALUES ('cundinamarca');
+
+INSERT INTO ciudad (nombre, fk_departamento) VALUES ('medellin', 1);
+INSERT INTO ciudad (nombre, fk_departamento) VALUES ('cali', 2);
+INSERT INTO ciudad (nombre, fk_departamento) VALUES ('bogota',3);
+
+INSERT INTO barrio (nombre, fk_ciudad) VALUES ('general', 1);
+INSERT INTO barrio (nombre, fk_ciudad) VALUES ('general', 2);
+INSERT INTO barrio (nombre, fk_ciudad) VALUES ('general', 3);
+
+
 
 
 
