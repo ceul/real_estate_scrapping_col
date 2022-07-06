655f786ada99015c88eb8e5852a4e289c7c021fc
“currego” <currego@splunk.com>
2022-07-06 10:07:10 -0500

    se eliminan endpoints inecesarios

--- src/core/controllers/propiedad/propiedad.controller.ts
+++ src/core/controllers/propiedad/propiedad.controller.ts
@@ -1,48 +1,34 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post, Query } from '@nestjs/common';
 import { PropiedadService } from '../../business/propiedad-service';
 import { CreatePropiedadDTO } from '../../models/create-propiedad.dto';
 
 @Controller('/propiedad')
 export class PropiedadController {
 
-    constructor(private propiedadService: PropiedadService) {}
+    constructor(private propiedadService: PropiedadService) { }
 
-    @Get(':id')
-    findOne(@Param('id') id){
+    @Get()
+    findOne(@Query('id') id) {
         return this.propiedadService.findOne(id)
     }
 
     @Get()
-    findAll(){
+    findAll() {
         return this.propiedadService.findAll()
     }
 
-    @Get('mean/:ciudad')
-    mean(@Param('ciudad') ciudad){
-        return this.propiedadService.mean(ciudad)
+    @Get('mean')
+    mean(@Query() params) {
+        return this.propiedadService.mean(params)
     }
 
     @Get()
-    median(){
+    median() {
         return this.propiedadService.findAll()
     }
 
     @Get()
-    mode(){
+    mode() {
         return this.propiedadService.findAll()
     }
-    
-    @Post()
-    create(@Body() input: CreatePropiedadDTO){
-        return input
-    }
-    
-    @Patch(':id')
-    update(@Param('id') id){
-
-    }
-    
-    @Delete(':id')
-    @HttpCode(204)
-    remove(@Param('id') id){}
 }
