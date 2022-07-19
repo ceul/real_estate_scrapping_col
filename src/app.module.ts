c36d90a86a8d86b6a2983066ec2fc0fdd954c926
ceul <ceul@ceul.ceul>
2022-07-18 20:06:17 -0500

    se habilita la coneccion ssl de psql

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,7 +26,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         database: configService.get('DB_NAME'),
         autoLoadEntities: true,
         entities: [Barrio, Ciudad, Departamento, Plataforma, TipoNegocio, TipoPropiedad, Propiedad],
-        synchronize: false
+        synchronize: false,
+        ssl: true
       }),
       inject: [ConfigService],
     }),
